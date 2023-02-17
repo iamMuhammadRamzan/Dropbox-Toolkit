01e5723d17ac7689b58b94b32386e18ccca80fa9
iamMuhammadRamzan <125671947+iamMuhammadRamzan@users.noreply.github.com>
2023-02-17 20:45:26 +0500

    Update main

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 from utils.dropbox_toolkit import DropboxToolkit
 
 f = open ('dropbox_credentials.json', "r")  
-    credentials = json.loads(f.read())
-    dbx = DropboxToolkit(credentials)
-    
+credentials = json.loads(f.read())
+dbx = DropboxToolkit(credentials)
 # do whatever you want to do
