1a123c4f4430e9366a93bc1a26a37be55c230399
iamMuhammadRamzan <125671947+iamMuhammadRamzan@users.noreply.github.com>
2023-02-20 13:59:47 +0500

    Remove extra code

--- utils/dropbox_utils.py
+++ utils/dropbox_utils.py
@@ -15,18 +15,16 @@ class DropboxToolkit:
         except Exception as e:
             print('Error connecting to Dropbox with access token: ' + str(e))
 
-    def dropbox_download_file(self, dropbox_file_path, local_file_path):
+    def download_file(self, dropbox_file_path, local_file_path):
         """Download a file from Dropbox to the local machine."""
         try:
-            fd, path = tempfile.mkstemp()
-            with open(path, 'wb') as f:
-                metadata, result = self.dbx.files_download(path=dropbox_file_path)
+            with open(local_file_path, 'wb') as f:
+                metadata, result = dbx.files_download(path=dropbox_file_path)
                 f.write(result.content)
-                return path
         except Exception as e:
             print('Error downloading file from Dropbox: ' + str(e))
 
-    def dropbox_list_files(self, path):
+    def list_files(self, path):
         try:
             files = self.dbx.files_list_folder(path).entries
             files_list = []
@@ -37,7 +35,7 @@ class DropboxToolkit:
         except Exception as e:
             print('Error getting list of files from Dropbox: ' + str(e))
 
-    def dropbox_list_folders(self, path):
+    def list_folders(self, path):
         try:
             folders = self.dbx.files_list_folder(path).entries
             folders_list = []
@@ -47,10 +45,10 @@ class DropboxToolkit:
         except Exception as e:
             print('Error getting list of folders from Dropbox: ' + str(e))
             
-    def dropbox_delete_file(self ,path):
+    def delete_file(self ,path):
         self.dbx.files_delete(path)
     
-    def dropbox_create_folder(self ,path):
+    def create_folder(self ,path):
         self.dbx.files_create_folder_v2(path)
 
     def download_to_tempfile(self, dropbox_file_path):
@@ -64,10 +62,5 @@ class DropboxToolkit:
         except Exception as e:
             print('Error downloading file from Dropbox: ' + str(e))
 
-    def dropbox_upload_image(self, content, upload_path):
+    def upload_file(self, content, upload_path):
         self.dbx.files_upload(content, upload_path)
-
-            
-            
-            
-
